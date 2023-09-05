8426164bffa9dbbe71d85b955a9c2d43cd5f1d32
cristian <cristian.ivanoff@gmail.com>
2023-09-05 20:57:08 +0200

    added array for airports and loop to extract all data from airports

--- pipelines/opensky.py
+++ pipelines/opensky.py
@@ -49,7 +49,8 @@ def main():
         Column("hashed", String, primary_key=True)
     )
     
-    airport = "EPWA" #"ESSA" # ICAO: Arlanda Stockholm -- IATA names are not used.
+    #airport = "EPWA" #"ESSA" # ICAO: Arlanda Stockholm -- IATA names are not used.
+    airports = ["EPKK","EPWA"] # EPWA = Warzaw, EPKK = Krakow
     default_begin_timestamp = datetime.datetime(2023,8,1,0,0, tzinfo=pytz.utc)
     end_timestamp = datetime.datetime.now(pytz.utc).replace(microsecond = 0)
     arrival = "arrival"
@@ -59,20 +60,27 @@ def main():
     
     #extract the data
     print(f"Begin time is: {begin_timestamp} and end time is: {end_timestamp}")
-    df_arrivals = extract_by_direction(opensky_client = opensky_client, direction = arrival, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
-    df_departures = extract_by_direction(opensky_client = opensky_client, direction = departure, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
-    
-    if isinstance(df_arrivals, pd.DataFrame):
-        print(f"Loading {arrival} data to database")
-        load(df=df_arrivals,postgresql_client=postgresql_client, table=table, metadata=metadata)
-    else:
-        print(f"No data to extract for direction: {arrival}")
 
-    if isinstance(df_departures, pd.DataFrame):
-        print(f"Loading {departure} data to database")
-        load(df=df_departures,postgresql_client=postgresql_client, table=table, metadata=metadata)
-    else:
-        print(f"No data to extract for direction: {departure}")
+    for airport in airports:
+        print(f"Extracting and loading for airport: {airport}")
+        df_arrivals = extract_by_direction(opensky_client = opensky_client, direction = arrival, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
+        df_departures = extract_by_direction(opensky_client = opensky_client, direction = departure, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
+        # end extract
+
+        #load flightdata to database
+        if isinstance(df_arrivals, pd.DataFrame):
+            print(f"Loading {arrival} data to database")
+            load(df=df_arrivals,postgresql_client=postgresql_client, table=table, metadata=metadata)
+        else:
+            print(f"No data to extract for direction: {arrival}")
+
+        if isinstance(df_departures, pd.DataFrame):
+            print(f"Loading {departure} data to database")
+            load(df=df_departures,postgresql_client=postgresql_client, table=table, metadata=metadata)
+        else:
+            print(f"No data to extract for direction: {departure}")
+        
+        # end load to database
 
 if __name__ == "__main__":
     main()
