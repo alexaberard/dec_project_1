97baa0cdea7b32a9113fc1aadd15c743c2b0eaf9
pdymek <pd37300@gmail.com>
2023-09-06 00:00:43 +0200

    added transform functions

--- pipelines/opensky.py
+++ pipelines/opensky.py
@@ -2,10 +2,13 @@ import os
 from connectors.open_sky_api_client import OpenSkyAPIClient
 from connectors.postgresql import PostgreSqlClient
 from assets.opensky import  load, extract_max_date, extract_by_direction
+from assets.transform import transform
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import pandas as pd
-from sqlalchemy import Table, Column, Integer, String, MetaData, Float, DateTime 
+from sqlalchemy.engine import URL, Engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, Float, DateTime, create_engine
+from jinja2 import Environment, FileSystemLoader, Template
 import pytz
 
 def main():
@@ -59,28 +62,57 @@ def main():
     begin_timestamp = extract_max_date(postgresql_client = postgresql_client, table = table, default_begin_timestamp = default_begin_timestamp)
     
     #extract the data
-    print(f"Begin time is: {begin_timestamp} and end time is: {end_timestamp}")
+    # print(f"Begin time is: {begin_timestamp} and end time is: {end_timestamp}")
 
-    for airport in airports:
-        print(f"Extracting and loading for airport: {airport}")
-        df_arrivals = extract_by_direction(opensky_client = opensky_client, direction = arrival, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
-        df_departures = extract_by_direction(opensky_client = opensky_client, direction = departure, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
-        # end extract
+    # for airport in airports:
+    #     print(f"Extracting and loading for airport: {airport}")
+    #     df_arrivals = extract_by_direction(opensky_client = opensky_client, direction = arrival, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
+    #     df_departures = extract_by_direction(opensky_client = opensky_client, direction = departure, airport = airport, begin_timestamp = begin_timestamp, end_timestamp = end_timestamp)
+    #     # end extract
 
-        #load flightdata to database
-        if isinstance(df_arrivals, pd.DataFrame):
-            print(f"Loading {arrival} data to database")
-            load(df=df_arrivals,postgresql_client=postgresql_client, table=table, metadata=metadata)
-        else:
-            print(f"No data to load for direction: {arrival}")
+    #     #load flightdata to database
+    #     if isinstance(df_arrivals, pd.DataFrame):
+    #         print(f"Loading {arrival} data to database")
+    #         load(df=df_arrivals,postgresql_client=postgresql_client, table=table, metadata=metadata)
+    #     else:
+    #         print(f"No data to load for direction: {arrival}")
 
-        if isinstance(df_departures, pd.DataFrame):
-            print(f"Loading {departure} data to database")
-            load(df=df_departures,postgresql_client=postgresql_client, table=table, metadata=metadata)
-        else:
-            print(f"No data to load for direction: {departure}")
+    #     if isinstance(df_departures, pd.DataFrame):
+    #         print(f"Loading {departure} data to database")
+    #         load(df=df_departures,postgresql_client=postgresql_client, table=table, metadata=metadata)
+    #     else:
+    #         print(f"No data to load for direction: {departure}")
         
-        # end load to database
+    # end load to database
+        
+    # start of transform
+    
+    dwh_connection_url = URL.create(
+        drivername = "postgresql+pg8000", 
+        username = DB_USERNAME,
+        password = DB_PASSWORD,
+        host = SERVER_NAME, 
+        # port = SERVER_PORT,
+        database = DATABASE_DWH_NAME, 
+    )
+    dwh_engine = create_engine(dwh_connection_url)
+    
+    transform_file_system_loader = FileSystemLoader("sql/transform")
+    transform_environment = Environment(loader=transform_file_system_loader)
+    
+    for sql_transform_file in transform_environment.list_templates():
+        print(sql_transform_file)
+        # sql_transform_template = transform_environment.get_template(sql_transform_file)
+        # # staging_table_name = sql_transform_template.make_module().config.get("table_name")
+        # staging_table_name = sql_transform_template.make_module().config.get("table_name")
+        # transform(
+        #     engine=dwh_engine,
+        #     sql_template=sql_transform_template,
+        #     table_name=staging_table_name,
+        # )  
+    
+    # end of transform
+        
 
 if __name__ == "__main__":
     main()
